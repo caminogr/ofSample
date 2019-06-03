77667fa8cf40e081f7b87060aa94ec3456d156d2
kaminora <kaminoura9207@gmail.com>
2019-06-03 15:21:25 +0900

    Change data to send

--- node/main.js
+++ node/main.js
@@ -3,6 +3,7 @@
 const client = new osc.Client('127.0.0.1', 8000);
  
 setInterval(() => {
-  client.send('/hoge', "Hello!");
+  // send (address, x, y, eventType);
+  client.send('/mouse/button', 50, 90, "down");
   console.log("messege send");
 }, 5000);
