295676da8fbe578a40bfc1ec491c9e77f3c48b19
kaminora <kaminoura9207@gmail.com>
2019-07-06 10:33:00 +0900

    Format code

--- OscInputText/src/ofApp.h
+++ OscInputText/src/ofApp.h
@@ -1,37 +1,36 @@
-#pragma once 
+#pragma once
 #include "ofMain.h"
-#include "ofxOsc.h"
 #include "ofxBox2d.h"
+#include "ofxOsc.h"
 
 //ポート番号を設定
 #define PORT 8000
 
 //——————————————————–
-class ofApp : public ofBaseApp{
-    
+class ofApp : public ofBaseApp {
+
 public:
-    
-    void setup();
-    void update();
-    void draw();
-    
-    void keyPressed (int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dumpOSC(ofxOscMessage m); //OSCメッセージを出力
+  void setup();
+  void update();
+  void draw();
+
+  void keyPressed(int key);
+  void mouseMoved(int x, int y);
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dumpOSC(ofxOscMessage m); // OSCメッセージを出力
 
 private:
-    //OSCメッセージを受信するインスタンス
-    ofxOscReceiver    receiver;
-    ofxBox2d box2d;
-    vector<shared_ptr<ofxBox2dCircle>> circles;
-    vector<ofPath> paths;
-    ofTrueTypeFont font;
-    string postedText;
-    string word;
+  // OSCメッセージを受信するインスタンス
+  ofxOscReceiver receiver;
+  ofxBox2d box2d;
+  vector<shared_ptr<ofxBox2dCircle>> circles;
+  vector<ofPath> paths;
+  ofTrueTypeFont font;
+  string postedText;
+  string word;
 
-    void drawPolyline(ofPolyline polyline);
+  void drawPolyline(ofPolyline polyline);
 };
